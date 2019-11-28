f18f0670e231727bc68c06b507bc29e114a73117
LisaGotzian <l-gotzian@t-online.de>
2019-11-28 17:55:55 +0100

    rewrote the error handling with ArgumentCheck() and inserted keepWordsFile (former moritzFile), left out 16 characters max threshold for words

--- R/2_processMetaDataMatrix.R
+++ R/2_processMetaDataMatrix.R
@@ -13,25 +13,71 @@
 #' @param control a list of parameters used to determine pre-processing methods. 
 #' Error will be thrown if language & stemWords are not defined. 
 #' e.g. control = list(language = "SMART", stemWords = FALSE)
+#' @param keepWordsFile a .csv-file that specifies which words to keep during the analysis. If provided, all other words will be disregarded.
 #' @family ginko functions
 
-processMetaDataMatrix <- function(metaMatrix, control = list()){
+processMetaDataMatrix <- function(metaMatrix, control = list(),
+                                  ignoreWords = c(),
+                                  keepWordsFile){
   
-  # Users have to define methods to be used in text pre-processing 
-  if(is.null(control$language))
-    stop("Please define which language to be used in control list!")
-  if(is.null(control$stemWords))
-    stop("Please define the stemWords in control list!")
+  ######## Argument checks
+  #* Establish a new 'ArgCheck' object
+  Check <- ArgumentCheck::newArgCheck()
   
-  stopifnot(is.list(control))
+  ## control arguments (the list of arguments we intend the user to define)
+  if(!is.list(control)) # if it's not a list, output error
+    ArgumentCheck::addError(
+      msg = "Please define the arguments of 'control' as a list!",
+      argcheck = Check
+      
+    )else{ # if it's a list check if it has been specified correctly
+      
+      # Users have to define methods to be used in text pre-processing 
+      if(is.null(control$language))
+        ArgumentCheck::addError(
+          msg = "Please define which language to be used in control list!",
+          argcheck = Check
+        )
+      if(is.null(control$stemWords))
+        ArgumentCheck::addError(
+          msg = "Please define the stemWords in control list!",
+          argcheck = Check
+        )
+    }
   
-  #Error handling
-  if (any(duplicated(metaMatrix[, "ID"]))) {
-    Sys.sleep(1)
-    print("Warning: entries do not have unique IDs, assigning new ones")
-    Sys.sleep(1)
-    
+  ## metaMatrix warnings
+  if (any(duplicated(metaMatrix[, "ID"]))){
+    ArgumentCheck::addWarning(
+      msg = "Entries do not have unique IDs, assigning new ones",
+      argcheck = Check
+    )
     metaMatrix[, "ID"] <- 1:length(metaMatrix[, "ID"])
+  }
+  
+  ## keepWordsFile errors
+  if(methods::hasArg(keepWordsFile)) # if the argument has been specified
+    if(!file.exists(keepWordsFile)) # but the file path doesn't exist
+      ArgumentCheck::addError(
+        msg = "If you'd like to use a file to specify the words to keep, please provide a correct file path.",
+        argcheck = Check
+      )
+  
+  #* Return errors and warnings (if any)
+  ArgumentCheck::finishArgCheck(Check)
+  
+  
+  # read the keepWordsFile in correctly as a csv or csv2
+  if (methods::hasArg(keepWordsFile)) { # if the argument has been specified
+    keepWords <- utils::read.csv(file = keepWordsFile) # it's a csv
+    if (grepl(";", keepWords[1, ])) {
+      #if a semicolon is in there...
+      keepWords <- utils::read.csv2(file = keepWordsFile) #it's a csv2
+    }
+    if (nrow(keepWords) < 10) {
+      stop(
+        "Please provide the .csv-file with an appropriate number of words in the following format:\n\nwords;select\nabandon;1\nabbott;0\n...\n(',' instead of ';' is fine as well.)\n\nor just have a list of words you'd like to keep."
+      )
+    }
   }
   
   ### This part checks for results without abstract or fulltext and excludes them
@@ -86,6 +132,15 @@ processMetaDataMatrix <- function(metaMatrix, control = list()){
   colnames(tf_idf) <- rownames(tf)
   tf_idf <- tf_idf /sqrt(rowSums(tf_idf^2)) # normalize doc vector to address bias in long doc
   tf_idf <- tf_idf[, colSums(tf_idf) != 0] # omit non-representative words 
+  
+  # Apply keepWordsFile if specified
+  if (methods::hasArg(keepWordsFile)) {
+    # keep only the words denoted with "1" if two columns are given
+    if (ncol(keepWords) == 2){keepWordsVector <- keepWords[keepWords[, 2] == 1, 1]}else{
+      keepWordsVector = keepWords
+    }
+    tf_idf <- tf_idf[,keepWordsVector]
+  }
   
   processedData <- list()
   processedData[[1]] <- tf_idf
