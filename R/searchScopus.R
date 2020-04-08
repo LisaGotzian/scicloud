0455fed6b57e1544ad52181714a03fc2187b50f7
JJuliusB <j.beeck@t-online.de>
2020-04-08 18:16:19 +0200

    Enabling more than 200 search results.

--- R/searchScopus.R
+++ R/searchScopus.R
@@ -13,7 +13,8 @@
 #'     Scopus does not provide more than 5000 results.
 #' @param countIncrement The number of results per GET request. A private user
 #'     can't exeed 25 per request. If you are inside a subscribed IP range,
-#'     you can use the maximum of 200 per request.
+#'     you can use the maximum of 200 per request. Note, that the weekly quota
+#'     for requests is 20,000.
 #' @param myAPIKey Your private Elsevier API key for the server communication.
 #'     You can request one at \url{https://dev.elsevier.com/}.
 #' @param saveToWd a logical parameter whether or not to save the output of the
@@ -26,6 +27,7 @@
 #' DOInumbers <- searchScopus('TITLE-ABS-KEY(sustainability) AND PUBYEAR > 2009',
 #'     '1234567890ABCDEF', maxResults = 160, countIncrement = 20)
 #' DOInumbers}
+
 searchScopus <- function(searchString,
                          myAPIKey,
                          maxResults = 10,
@@ -36,22 +38,24 @@ searchScopus <- function(searchString,
   # percent-encode the search string
   searchString <- utils::URLencode(searchString)
   
-  # initialize an empty results object
-  searchResults <- NULL
+  # initialize an empty request results object
+  searchEntries <- NULL
   
-  start <- 1
+  # initialize start parameter for API call
+  start <- 0
   
   # initialize the progress bar utility
-  pb <- utils::txtProgressBar(min = start, max = maxResults, style = 3)
+  pb <-
+    utils::txtProgressBar(min = start, max = maxResults, style = 3)
   
   # a function that returns a custom error message
   errorMessage <- function(cond) {
     message(
       paste0(
-        "Error in while retrieving the title - DOI: ",
-        intermediateSearchResults[i, "DOI"],
+        "Error while retrieving the title - DOI: ",
+        searchResults[i, "DOI"],
         ", Scopus-ID: ",
-        intermediateSearchResults[i, "Scopus-ID"]
+        searchResults[i, "Scopus-ID"]
       )
     )
     message("Here's the original error message:")
@@ -63,10 +67,10 @@ searchScopus <- function(searchString,
   warningMessage <- function(cond) {
     message(
       paste0(
-        "Warning in while retrieving the title - DOI: ",
-        intermediateSearchResults[i, "DOI"],
+        "Warning while retrieving the title - DOI: ",
+        searchResults[i, "DOI"],
         ", Scopus-ID: ",
-        intermediateSearchResults[i, "Scopus-ID"]
+        searchResults[i, "Scopus-ID"]
       )
     )
     message("Here's the original warning message:")
@@ -75,7 +79,20 @@ searchScopus <- function(searchString,
     return(NULL)
   }
   
-  while (start < maxResults) {
+  # evaluation of the number of requests in order to be
+  # able to get more than 200 results
+  numberOfRequests <- ceiling(maxResults / countIncrement)
+  
+  if (maxResults > 5000 | numberOfRequests > 20000) {
+    stop("Scopus does not provide more than 5,000 search results or 20,000 requests.")
+  }
+  
+  for (i in 1:numberOfRequests) {
+    # store only maxResults entries
+    if (i == numberOfRequests) {
+      countIncrement <- maxResults %% countIncrement
+    }
+    
     # create the url that is sent as a GET request
     URL <-
       paste0(
@@ -93,87 +110,92 @@ searchScopus <- function(searchString,
                 httr::add_headers(`X-ELS-APIKey` = myAPIKey, Accept = "application/json"))
     
     # store the JSON content from the response
-    JSON <- httr::content(serverResponse)
+    responseContent <- httr::content(serverResponse)
     
-    # update the current number of results
-    start = start + countIncrement
+    # store JSON content in entry vector
+    searchEntries <-
+      c(searchEntries, responseContent$`search-results`$entry)
     
-    numberOfEntries <-
-      maxResults  #length(JSON$`search-results`$entry)
-    # numberOfEntries = length(JSON$`search-results`$entry) # ideally, this should work
-    
-    # initialize a matrix that stores the results (more efficient this way)
-    intermediateSearchResults <-
-      matrix(NA, nrow = numberOfEntries, ncol = 20)
-    # assign column header names to the matrix where we store the results
-    colnames(intermediateSearchResults) <-
-      c(
-        "Title",
-        "Year",
-        "Month",
-        "Day",
-        "Authors",
-        "Journal",
-        "Volume",
-        "Issue",
-        "Pages",
-        "CitedBy",
-        "CitationPerYear",
-        "DOI",
-        "Scopus-ID",
-        "Publisher",
-        "Affiliation",
-        "Affiliation-City",
-        "Affiliation-Country",
-        "FileName",
-        "Abstract",
-        "FullText"
-      )
-    
-    # let users know what is happening at this stage (assissted with progress bar later)
-    cat("Accessing DOIs and Scopus IDs of the search result...\n")
-    
-    # attempt to grab the contents that we are interested in
-    for (i in 1:numberOfEntries) {
-      # grab the DOI
-      intermediateResultDOI <- tryCatch({
-        JSON$`search-results`$entry[[i]]$`prism:doi`
-      }, error = errorMessage, warning = warningMessage)
-      
-      # Grab the scopus ID
-      intermediateResultScopusID <- tryCatch({
-        intermediateResultScopusID <-
-          JSON$`search-results`$entry[[i]]$`dc:identifier`
-        intermediateResultScopusID <-
-          sub("SCOPUS_ID:", "", intermediateResultScopusID)
-      }, error = errorMessage, warning = warningMessage)
-      
-      # store DOI and Scopus ID
-      intermediateSearchResults[i, "DOI"] <-
-        if (length(intermediateResultDOI) > 0) {
-          intermediateResultDOI
-        } else {
-          NA
-        }
-      intermediateSearchResults[i, "Scopus-ID"] <-
-        if (length(intermediateResultScopusID) > 0) {
-          intermediateResultScopusID
-        } else {
-          NA
-        }
-      
-    }
-    
-    # add intermediateSearchResults to searchResults as a new row
-    searchResults <- rbind(searchResults, intermediateSearchResults)
-    
-    # save metaDOInumbers dataFrame to R object file to working directory & global env
-    if (saveToWd == TRUE) {
-      save_data(searchResults, "metaDOInumbers")
-    }
+    # update the current number of results ensuring the progress bar always ends with 100%
+    start <- min(start + countIncrement, maxResults)
     
     # show progress Bar
     utils::setTxtProgressBar(pb, start)
+    
+    # API allows 9 requests per second
+    if (i %% 9 == 0) {
+      Sys.sleep(1)
+    }
+  }
+  
+  cat(
+    "\nRemaining quota:",
+    serverResponse$headers$`x-ratelimit-remaining`,
+    "requests within the next 7 days.\n"
+  )
+  Sys.sleep(1)
+  
+  # initialize a matrix that stores the results (more efficient this way)
+  searchResults <-
+    matrix(NA, nrow = maxResults, ncol = 20)
+  # assign column header names to the matrix where we store the results
+  colnames(searchResults) <-
+    c(
+      "Title",
+      "Year",
+      "Month",
+      "Day",
+      "Authors",
+      "Journal",
+      "Volume",
+      "Issue",
+      "Pages",
+      "CitedBy",
+      "CitationPerYear",
+      "DOI",
+      "Scopus-ID",
+      "Publisher",
+      "Affiliation",
+      "Affiliation-City",
+      "Affiliation-Country",
+      "FileName",
+      "Abstract",
+      "FullText"
+    )
+  
+  # let users know what is happening at this stage (assissted with progress bar later)
+  cat("\nAccessing DOIs and Scopus IDs of the search result...\n")
+  
+  # attempt to grab the contents that we are interested in
+  for (i in 1:maxResults) {
+    # grab the DOI
+    resultDOI <- tryCatch({
+      searchEntries[[i]]$`prism:doi`
+    }, error = errorMessage, warning = warningMessage)
+    
+    # Grab the scopus ID
+    resultScopusID <- tryCatch({
+      sub("SCOPUS_ID:", "", searchEntries[[i]]$`dc:identifier`)
+    }, error = errorMessage, warning = warningMessage)
+    
+    # store DOI and Scopus ID
+    searchResults[i, "DOI"] <-
+      if (length(resultDOI) > 0) {
+        resultDOI
+      } else {
+        NA
+      }
+    searchResults[i, "Scopus-ID"] <-
+      if (length(resultScopusID) > 0) {
+        resultScopusID
+      } else {
+        NA
+      }
+  }
+  
+  # save metaDOInumbers dataFrame to R object file to working directory & global env
+  if (saveToWd == TRUE) {
+    save_data(searchResults, "metaDOInumbers")
   }
   
   # check for redundant entries
@@ -181,9 +203,6 @@ searchScopus <- function(searchString,
   # assign unique ID to the rows to avoid any collisions along the way
   searchResults <- cbind(searchResults, ID = 1:nrow(searchResults))
   
-  # this ensures that the progress bar always ends with 100% (which might not happen due to possible
-  # missmatches between countIncrement and maxResults)
-  utils::setTxtProgressBar(pb, maxResults)
   close(pb)  # close progress bar
   
   return(searchResults)
